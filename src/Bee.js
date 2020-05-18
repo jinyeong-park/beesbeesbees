5de18daa423b1fa0b72d79814e5a05e1adbadab1
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-05-18 12:42:22 -0700

    completed Grub, Bees, HoneyMakerBee, ForagerBee, Retired

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,16 @@
 var Bee = function() {
+
+//copy over Grub (age, color,food )
+ Grub.call(this);
+ //and then modify some property
+ this.age = 5;
+ this.color = 'yellow';
+ this.job = 'keep on growing';
 };
+
+//get Grup.prototype
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+
+
 
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,22 @@
 var HoneyMakerBee = function() {
+  //copy over Grub (age, color,food )
+ Bee.call(this);
+ this.age = 10;
+//  this.color = 'yellow';
+ this.job = 'make honey';
+ this.honeyPot = 0;
+
 };
+
+//get Grup.prototype
+HoneyMakerBee.prototype = Object.create(Grub.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+}
+HoneyMakerBee.prototype.giveHoney =  function() {
+  this.honeyPot--;
+}
+
+
 
